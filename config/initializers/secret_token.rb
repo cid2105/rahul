b2aeef469650f9c32a12ebd6c61bfd25243335a6
Cole Diamond <cd1am0nd@yahoo.com>
2011-11-25 20:22:52 -0500

    asdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resume::Application.config.secret_token = '575d1fb927bced12c094971d68aa24c21a8ef162a5480ccc8dcc777eadb8195d685b64852519ec6f294afc2ad97db125d930689e8310eab76b2997ec27567518'
