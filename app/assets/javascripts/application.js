887cf913b29419be29686d679b95c3b9e53de715
Cole Diamond <cd1am0nd@yahoo.com>
2011-11-26 02:17:32 -0500

    asdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,14 +3,12 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//
-//= require jquery.min
+//= require_self
 //= require jquery
 //= require cufon
 //= require cufon.font
-//= require custom
-//= require cycle
 //= require jquery.prettyPhoto
 //= require vcard.packed
 
+//= require cycle
 
