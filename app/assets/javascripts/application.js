b399167599b82d23e74fb4f69786645d0448d1e2
Cole Diamond <cd1am0nd@yahoo.com>
2011-11-26 15:03:00 -0500

    asdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,17 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require jquery 
+//= require jquery_ui 
+//= require cufon
+//= require cufon.font 
+//= require jquery.prettyPhoto 
+//= require vcard.packed 
+//= require cycle
+//= require facebox 
+//= require mousewheel
+//= require easing 
+//= require fancybox
+//= require tipsy 
+//= require notification
 
